08da764d0c6669266441a06206d9903edba177f2
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-01-05 13:37:50 +0200

    Improvements added

--- src/components/FeedbackButton/FeedbackButton.jsx
+++ src/components/FeedbackButton/FeedbackButton.jsx
@@ -1,11 +1,7 @@
 import { StyledButton } from './FeedbackButton.styled';
 
 const FeedbackButton = ({ text, onClick }) => {
-  return (
-    <StyledButton onClick={onClick} data-feedback={text.toLowerCase()}>
-      {text}
-    </StyledButton>
-  );
+  return <StyledButton onClick={onClick}>{text}</StyledButton>;
 };
 
 export default FeedbackButton;
